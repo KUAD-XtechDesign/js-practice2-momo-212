ed047a8f82517a0609c86cd1a1fa75d23f0fa59a
momo-212 <68629342+momo-212@users.noreply.github.com>
2021-10-15 14:50:26 +0900

    Update script01.js

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,6 +1,52 @@
 $(function(){
 
 
+let txt1 = "おはようございます！";
+let txt2 = "おげんきですか？";
+let num1 = 20;
+let num2 = 32;
+
+
+    function sayhello(){
+    $("#content").text("こんにちは！");
+}
+
+function saysomething(txt){
+    $("#content").text(txt);
+}
+
+
+function samtwo(arg1,arg2){
+    $("#sontent").text(arg1+arg2);
+}
+
+
+
+$("#button01").on("click",function(){
+    sayhello();
+})
+
+
+$("#button02").on("click",function(){
+    saysomething(txt1);
+})
+
+
+$("#button03").on("click",function(){
+    samtwo(txt1,txt2);
+})
+
+
+$("#button04").on("click",function(){
+    samtwo(numi,num2);
+})
+
+
+$("#button05").on("click",function(){
+    samtwo(txt1,txt2);
+    samtwo(num1,num2);
+})
+
 
 
 })
