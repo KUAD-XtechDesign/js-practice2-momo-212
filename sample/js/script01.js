9b99697f6970aac37e416929f07eb218fbd7013c
momo-212 <68629342+momo-212@users.noreply.github.com>
2021-10-15 14:55:15 +0900

    Update script01.js

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -7,44 +7,44 @@ let num1 = 20;
 let num2 = 32;
 
 
-    function sayhello(){
+    function sayHello(){
     $("#content").text("こんにちは！");
 }
 
-function saysomething(txt){
+function saySomething(txt){
     $("#content").text(txt);
 }
 
 
-function samtwo(arg1,arg2){
+function samTwo(arg1,arg2){
     $("#sontent").text(arg1+arg2);
 }
 
 
 
 $("#button01").on("click",function(){
-    sayhello();
+    sayHello();
 })
 
 
 $("#button02").on("click",function(){
-    saysomething(txt1);
+    saySomething(txt1);
 })
 
 
 $("#button03").on("click",function(){
-    samtwo(txt1,txt2);
+    samTwo(txt1,txt2);
 })
 
 
 $("#button04").on("click",function(){
-    samtwo(numi,num2);
+    samTwo(numi,num2);
 })
 
 
 $("#button05").on("click",function(){
-    samtwo(txt1,txt2);
-    samtwo(num1,num2);
+    samTwo(txt1,txt2);
+    samTwo(num1,num2);
 })
 
 
